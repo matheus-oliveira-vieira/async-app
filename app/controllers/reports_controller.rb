89c032e665d42c28584aec8ed75fe18cf963e2bc
Matheus Oliveira <matheus.oliveirav@gmail.com>
2021-12-16 17:44:18 -0300

    Insert Report model

---
+++ app/controllers/reports_controller.rb
@@ -0,0 +1,12 @@
+class ReportsController < ApplicationController
+  def index
+  end
+
+  def new
+    @country = Country.new
+  end
+
+  def create
+    
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 require 'sidekiq/web'
 Rails.application.routes.draw do
+  root 'home#index'
+  resources :reports, only: %i[index create]
   mount Sidekiq::Web => '/sidekiq'
 end
 
